08a5a06c557479d1461b3e5f3e644d406086c140
GVGulov <ghulov@list.ru>
2024-02-06 22:57:30 +0300

    Добавлен редирект на localhost:3000 при успешной авторизации

--- knowledge-graph/src/main/kotlin/gvgulov/knowledgegraph/config/SecurityConfiguration.kt
+++ knowledge-graph/src/main/kotlin/gvgulov/knowledgegraph/config/SecurityConfiguration.kt
@@ -5,18 +5,12 @@ import gvgulov.knowledgegraph.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.config.Customizer.withDefaults
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.DefaultSecurityFilterChain
-import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
@@ -30,14 +24,13 @@ class SecurityConfiguration {
     fun filterChain(http: HttpSecurity): DefaultSecurityFilterChain {
         http.authorizeHttpRequests { authz ->
             authz
-                .requestMatchers("/auth/login", "error").permitAll()
-                .anyRequest()
-                .authenticated()
+                .requestMatchers("/auth/login").permitAll()
+                .anyRequest().authenticated()
         }.formLogin { loginConfigurer ->
             loginConfigurer
                 .loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
-                .defaultSuccessUrl("/index")
+                .defaultSuccessUrl("/auth/successfulLogin", true)
                 .failureUrl("/auth/login?error")
 
         }.csrf { it.disable() }
@@ -48,15 +41,4 @@ class SecurityConfiguration {
     @Bean
     fun passwordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
 
-//    @Bean
-//    fun daoAuthenticationProvider(): DaoAuthenticationProvider =
-//        DaoAuthenticationProvider().apply {
-//            setPasswordEncoder(passwordEncoder())
-//            setUserDetailsService(service)
-//        }
-//
-//
-//    @Bean
-//    fun authenticationManager(configuration: AuthenticationConfiguration): AuthenticationManager =
-//        configuration.authenticationManager
 }
--- knowledge-graph/src/main/kotlin/gvgulov/knowledgegraph/contollers/AuthController.kt
+++ knowledge-graph/src/main/kotlin/gvgulov/knowledgegraph/contollers/AuthController.kt
@@ -1,14 +1,21 @@
 package gvgulov.knowledgegraph.contollers
 
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
 
 @Controller
 @RequestMapping("/auth")
 class AuthController {
 
     @GetMapping("/login")
-    fun loginPage():String = "login"
+    fun loginPage(): String = "login"
+
+    @GetMapping("/successfulLogin")
+    fun successfulLogin(response: HttpServletResponse) = ModelAndView("redirect:http://localhost:3000");
 
 }
